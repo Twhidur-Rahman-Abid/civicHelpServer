6df683db7ca296453f9aeb8436fb8d0fd9fb7ff6
Towhidur-Rahman-Abid <abidrahman085@gmail.com>
2026-09-22 10:15:54 +0600

    add frontend url on crose origins

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],       
+    allow_origins=["http://localhost:5173","https://civichelpfrontend.vercel.app","https://civichelp.towhidur-rahman-abed.me"],       
     allow_credentials=True,      
     allow_methods=["*"],         
     allow_headers=["*"],       
